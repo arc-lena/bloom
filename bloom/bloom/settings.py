34d63c844b47a575d056383a3dd6a03bddcd06fa
Анастасія Лукянчук <anastasiia.lukianchuk@lnu.edu.ua>
2024-10-07 20:53:45 +0300

    homepage with issues

--- bloom/bloom/settings.py
+++ bloom/bloom/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'bloom.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],  # Вказуємо на глобальні шаблони'
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,3 +122,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [BASE_DIR / 'static']
+
