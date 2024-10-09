e5e36aa23f74a7451f8ccce62c8023600b4eaace
andriyko <blazietopgg@gmail.com>
2024-10-09 20:39:35 +0200

    changed file admin.py

--- bloom/tasks/admin.py
+++ bloom/tasks/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Task, TaskStatus
 
-# Register your models here.
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(TaskStatus)
+class TaskStatusAdmin(admin.ModelAdmin):
+    pass
--- bloom/tasks/models.py
+++ bloom/tasks/models.py
@@ -4,8 +4,12 @@ from django.contrib.auth.models import User
 class Task(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    completed = models.BooleanField(default=False)
+
 
     def __str__(self):
         return self.title
+
+class TaskStatus(models.Model):
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
+    completed = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
