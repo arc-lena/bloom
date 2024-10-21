5b33b98d923ac6beab742e7fab4fe3bdb1fb3e03
Solomia <solomiia.hartsula@lnu.edu.ua>
2024-10-21 18:47:58 +0300

    added settings front

--- bloom/profiles/urls.py
+++ bloom/profiles/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),  # Головна сторінка
+    path('', views.home, name='home'),
+    path('settings/', views.profile_settings_view, name='profile_set'),
 ]
