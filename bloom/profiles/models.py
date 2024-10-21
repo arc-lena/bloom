89dd4f16c3215611d85ad5dd379f051e9ff8e7cd
Solomia <solomiia.hartsula@lnu.edu.ua>
2024-10-21 18:10:34 +0300

    added simple profile page

--- bloom/profiles/models.py
+++ bloom/profiles/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    avatar = models.ImageField(upload_to='avatars/', default='avatars/default.png')
     level = models.IntegerField(default=0)
     points_balance = models.IntegerField(default=0)
     completed_tasks_count = models.IntegerField(default=0)
